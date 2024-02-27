2c72d824239caeb8c60369aff9869335eff49051
saoumsadat <sadatsaoum@gmail.com>
2024-02-27 23:58:41 +0600

    r

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
-a = np.array([[1, 2, 3]], dtype='int16')
-b = np.array([[1, 2, 3], [4, 5, 6]], dtype='int32')
-print(a.shape)
-print(b.shape)
-print(a.itemsize)
-print(b.itemsize)
+
+x= np.arange(5)
+y= np.array([0, 2, 4, 6, 8])
+print(x)
+print(y)
+
+plt.plot(x, y)
+plt.title("Graph Name")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+plt.xticks([0,1,2,3,4])
+plt.yticks([0,2,4,6,8])
+
+plt.grid()
+plt.show()
